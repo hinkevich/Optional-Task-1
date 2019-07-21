d36f811dfaf3f471e117ccb4cacb35e8600b7de8
hinkevich <51876492+hinkevich@users.noreply.github.com>
2019-07-21 17:47:17 +0300

    Add task 1

--- src/main/java/epam/learn/App.java
+++ src/main/java/epam/learn/App.java
@@ -15,40 +15,31 @@ public class App
 //        System.out.println( "Enter name:" );
 //        name=scanner.next();
 //        System.out.println( "Hello "+name );
-        //task1-2
-//        int quantityNumber;
-//        int[] arrays;
-//        System.out.println( "Enter quantity Numbers:" );
-//        quantityNumber=scanner.nextInt();
-//        arrays =new int[quantityNumber];
-//        for (int i=0; i<quantityNumber;i++) {
-//            System.out.print( "Enter Numbers #"+(i+1)+" " );
-//            arrays[i]=scanner.nextInt();
-//              }
-//        System.out.println( Arrays.toString(arrays) );
-//
-//        for (int i=0; i<arrays.length;i++) {
-//            System.out.print(arrays[arrays.length-(i+1)]+" " );
-//
-//        }
+
+       ?
         //task1-3
 
-        Random random = new Random();
-        int[] arraysNumber;
-        int sizeArray;
-        System.out.println( "Enter quantity Numbers:" );
-        sizeArray=scanner.nextInt();
-        arraysNumber=new int[sizeArray];
-        for (int i = 0; i <arraysNumber.length ; i++) {
-            arraysNumber[i]=random.nextInt(); 
-        }
-        for (int i = 0; i < ; i++) {
-            
-        }
-        
+//        Random random = new Random();
+//        int[] arraysNumber;
+//        int sizeArray;
+//        System.out.println( "Enter quantity Numbers:" );
+//        sizeArray=scanner.nextInt();
+//        arraysNumber=new int[sizeArray];
+//        for (int i = 0; i <arraysNumber.length ; i++) {
+//            arraysNumber[i]=random.nextInt(1000);
+//        }
+//        for (int i = 0; i <arraysNumber.length ; i++) {
+//            System.out.print(arraysNumber[i]+" " );
+//        }
+//        for (int i = 0; i < arraysNumber.length; i++) {
+//            System.out.print("\n"+arraysNumber[i]+" " );
+//
+//        }
 
 
 
+
+//
 
 
 
